98c7c5ecde089a06b022aeb0e9894e22d93ca344
Guidotti-dll <dev.lucasguidotti@gmail.com>
2020-04-14 15:18:17 -0300

    class five

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-class Matematica {
-    static soma(a,b) {
-        return a + b; 
+function teste(x) {
+    let y=2;
+
+    if (x>5) {
+        let y=4;
+
+        console.log(x,y);
     }
 }
 
-console.log(Matematica.soma(1,2));
-
-
-
-// class List {
-//     constructor() {
-//         this.data = [];
-//     }
-
-//     add(data) {
-//         this.data.push(data);
-//         console.log(this.data);
-//     }
-// }
-
-// class TodoList extends List {
-//     constructor() {
-//         super();
-
-//         this.usuario = 'Lucas';
-//     }
-
-//         mostraUsuario() {
-//             console.log(this.usuario);
-//         }
-//     }
-
-
-// const MinhaLista = new TodoList();
-
-// document.getElementById('novoTodo').onclick = function() {
-//     MinhaLista.add('Novo Todo');
-// }
-
-// MinhaLista.mostraUsuario();
+(teste(10));
