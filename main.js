08b19096b32b181c050cbbbc55ddc932f2d93ee3
Guidotti-dll <dev.lucasguidotti@gmail.com>
2020-04-14 14:43:36 -0300

    class four

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-
-
-class Teste {
-    metodo() {
-
-    }
-    outro() {
-
+class Matematica {
+    static soma(a,b) {
+        return a + b; 
     }
 }
+
+console.log(Matematica.soma(1,2));
+
+
+
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
+
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+
+// class TodoList extends List {
+//     constructor() {
+//         super();
+
+//         this.usuario = 'Lucas';
+//     }
+
+//         mostraUsuario() {
+//             console.log(this.usuario);
+//         }
+//     }
+
+
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novoTodo').onclick = function() {
+//     MinhaLista.add('Novo Todo');
+// }
+
+// MinhaLista.mostraUsuario();
